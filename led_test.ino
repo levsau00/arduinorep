92484d127eb33ef6dc1d0257e5af65c89c4292fb
levsau00 <levsau00@gmail.com>
2016-12-21 16:22:22 -0500

    Add files via upload

---
+++ led_test.ino
@@ -0,0 +1,18 @@
+int LED = 2;
+int as = 3;
+void setup() 
+{
+  pinMode(LED,OUTPUT);
+}
+
+void loop() 
+{
+ 
+  if(as == 2)
+  {
+  digitalWrite(LED,HIGH);
+  delay(1000);
+  digitalWrite(LED,LOW);
+  delay(1000);
+  }
+}
